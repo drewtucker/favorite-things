2beabba33b59ac5244c5018c296acb1d1f39cc14
Drew Tucker and Ernest Bruno <ernest@>
2018-01-16 09:11:50 -0800

    Add form to retrieve input data, format data into array, and diplay in different order

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#inputs").submit(function(event){
+    var input1 = $("input#fav1").val();
+    var input2 = $("input#fav2").val();
+    var input3 = $("input#fav3").val();
+    var input4 = $("input#fav4").val();
+    var input5 = $("input#fav5").val();
+
+    var array1 = [input1, input2, input3, input4, input5];
+    var array2 = [];
+    array2.push(array1[1]);
+    array2.push(array1[0]);
+    array2.push(array1[2]);
+
+    $("ul#results").append("<li>" + array2[0] + "</li>");
+    $("ul#results").append("<li>" + array2[1] + "</li>");
+    $("ul#results").append("<li>" + array2[2] + "</li>");
+
+    event.preventDefault();
+  });
+});
